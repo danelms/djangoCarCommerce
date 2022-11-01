4a9495f079e56746fe90fbd8e464008b8e001fc8
danelms <74295308+danelms@users.noreply.github.com>
2022-11-01 16:47:29 +0000

    Models implemented

--- carCommerce_project/carComm_app/admin.py
+++ carCommerce_project/carComm_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Listing
+from .models import ListingImage
 
-# Register your models here.
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ListingImage)
+class ListingImageAdmin(admin.ModelAdmin):
+    pass
--- carCommerce_project/carComm_app/models.py
+++ carCommerce_project/carComm_app/models.py
@@ -1,3 +1,26 @@
+import datetime
+import string
+import time
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+currentDate = datetime.date.today()
+currentYear = currentDate.year
+
+class Listing(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    registration = models.CharField(max_length=10)
+    colour = models.CharField(max_length=50)
+    year = models.IntegerField(validators=[MaxValueValidator(currentYear), MinValueValidator(1769)])
+    thumbnail = models.ImageField(upload_to='images/')
+    description = models.TextField(max_length=2500)
+    price = models.FloatField(validators=[MinValueValidator(0.01)])
+    
+    def __str__(self) -> str:
+        return self.make + " " + self.model + ", " + self.registration
+
+class ListingImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    Listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
