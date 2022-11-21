f35d5b1594fd75a20e112578828d1e4b6aa08576
danelms <danelms@live.co.uk>
2022-11-21 10:56:06 +0000

    contact progress (doesn't work)

--- carCommerce_project/carComm_app/views.py
+++ carCommerce_project/carComm_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
+from django.core.mail import send_mail, BadHeaderError
+from .forms import ContactForm
 from .models import Listing, ListingImage, Queries
 
 def home(request):
@@ -13,7 +15,26 @@ def listings(request):
     return render(request, 'listings.html', {'listings':listings, 'uniqueColours':uniqueColours, 'uniqueMakes':uniqueMakes})
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            body = {
+            'name': form.cleaned_data['name'],
+            'email': form.cleaned_data['email_address'],
+            'subject': form.cleaned_data['subject'],
+            'message':form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(message, 'danelms@live.co.uk', ['danelms@live.co.uk'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+        else:
+            return redirect (request, "home.html")
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'contactForm': form})
 
 def carDetails(request, registration):
     try:
